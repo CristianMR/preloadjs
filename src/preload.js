51a85e21df906c1dcfd86ad0cef1e4e78987ee49
HeberLZ <heberlopez@gmail.com>
2014-11-22 21:11:59 -0300

    fix in remove version replaced jQuery ajax with AngularJS's httpBackend

--- src/preload.js
+++ src/preload.js
@@ -34,7 +34,7 @@
         var callbackModList = {};
 
         //Delete scripts in localStorage, if not equals version of app
-        if(storage[withPrefix('version')] && parseFloat(storage[withPrefix('version')]) !== version && storage[withPrefix('map')]){
+        if(storage[withPrefix('version')] && parseFloat(storage[withPrefix('version')]) !== version){
           _.forIn(localStorage, function(value, key){
             //Checks whether the key starts with "pl_", if it does, removes the item from the localStorage
             if(/^pl_.+$/.test(key)){
@@ -71,11 +71,12 @@
         }
 
         function getScript(url, done, module){
-            $.get(url, function(data) {
-                console.log("url load", url);
-                storage[withPrefix(url)] = data;
-                done(module);
-            }, 'text');//Type text, then jQuery don't try to execute it.
+          ajax('GET', url, function(status, data){
+            if(status !== 200) return done(module);
+            console.log("url load", url);
+            storage[withPrefix(url)] = data;
+            done(module);
+          });
         }
 
         function initModule(name){//TODO add external visibility
@@ -133,6 +134,86 @@
         return 'pl_' + string;
       }
 
+    }
+
+    /**
+     * Copyright (c) 2010-2014 Google, Inc. http://angularjs.org | MIT
+     * taken from https://github.com/angular/angular.js/blob/master/src/ng/httpBackend.js
+     */
+    function ajax(method, url, callback, post, headers, withCredentials, responseType) {
+
+      responseType = responseType ? responseType : 'text';
+
+      var xhr = new window.XMLHttpRequest();
+
+      xhr.open(method, url, true);
+      _.forIn(headers, function(value, key) {
+        if (!_.isUndefined(value)) {
+          xhr.setRequestHeader(key, value);
+        }
+      });
+
+      xhr.onload = function requestLoaded() {
+        var statusText = xhr.statusText || '';
+
+        // responseText is the old-school way of retrieving response (supported by IE8 & 9)
+        // response/responseType properties were introduced in XHR Level2 spec (supported by IE10)
+        var response = ('response' in xhr) ? xhr.response : xhr.responseText;
+
+        // normalize IE9 bug (http://bugs.jquery.com/ticket/1450)
+        var status = xhr.status === 1223 ? 204 : xhr.status;
+
+        // fix status code when it is 0 (0 status is undocumented).
+        // Occurs when accessing file resources or on Android 4.1 stock browser
+        // while retrieving files from application cache.
+        if (status === 0) {
+          status = response ? 200 : /^file.+$/.test(url) ? 404 : 0;
+        }
+
+        completeRequest(callback,
+            status,
+            response,
+            xhr.getAllResponseHeaders(),
+            statusText);
+      };
+
+      var requestError = function() {
+        // The response is always empty
+        // See https://xhr.spec.whatwg.org/#request-error-steps and https://fetch.spec.whatwg.org/#concept-network-error
+        completeRequest(callback, -1, null, null, '');
+      };
+
+      xhr.onerror = requestError;
+      xhr.onabort = requestError;
+
+      if (withCredentials) {
+        xhr.withCredentials = true;
+      }
+
+      if (responseType) {
+        try {
+          xhr.responseType = responseType;
+        } catch (e) {
+          // WebKit added support for the json responseType value on 09/03/2013
+          // https://bugs.webkit.org/show_bug.cgi?id=73648. Versions of Safari prior to 7 are
+          // known to throw when setting the value "json" as the response type. Other older
+          // browsers implementing the responseType
+          //
+          // The json response type can be ignored if not supported, because JSON payloads are
+          // parsed on the client-side regardless.
+          if (responseType !== 'json') {
+            throw e;
+          }
+        }
+      }
+
+      xhr.send(post || null);
+
+      function completeRequest(callback, status, response, headersString, statusText) {
+        xhr = null;
+
+        callback(status, response, headersString, statusText);
+      }
     }
 
     window.preload = preload;
